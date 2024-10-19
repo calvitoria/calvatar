1ec7bac0c8cdd035c0c895cb9620720e4fb9cb0c
Vitória Calvi <calvi.vitoria@gmail.com>
2024-10-19 16:15:09 -0300

    setting things up

---
+++ lib/calvatar.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "calvatar/version"
+
+module Calvatar
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/calvatar/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Calvatar
+  VERSION = "0.1.0"
+end

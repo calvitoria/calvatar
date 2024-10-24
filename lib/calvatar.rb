cd5a9438f7b2c584a19db844a696436b0b030b14
Vitória Calvi <calvi.vitoria@gmail.com>
2024-10-24 09:19:47 -0300

    update gem to be able to receive values from user

--- lib/calvatar.rb
+++ lib/calvatar.rb
@@ -5,34 +5,37 @@ require 'calvatar_helper'
 
 module Calvatar
   class Avatar
-    COLORS = [
+    DEFAULT_COLORS = [
       '#FF5733', '#33FF57', '#3357FF', '#F1C40F', '#9B59B6',
       '#E67E22', '#1ABC9C', '#2ECC71', '#3498DB', '#34495E'
-    ]
+    ].freeze
 
-    def initialize(name)
+    DEFAULT_SIZE = 50
+
+    def initialize(name, colors: DEFAULT_COLORS, size: DEFAULT_SIZE)
       @initials = extract_initials(name)
-      @color = COLORS.sample
+      @color = colors.sample
+      @size = size
     end
 
     def generate
       svg = <<~SVG
-        <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'>
-          <rect width='100' height='100' fill='#{@color}'/>
-          <text x='50%' y='50%' font-size='40' text-anchor='middle' fill='white' dy='.3em'>#{@initials}</text>
+        <svg xmlns='http://www.w3.org/2000/svg' width='#{@size}' height='#{@size}'>
+          <rect width='100%' height='100%' fill='#{@color}'/>
+          <text x='50%' y='50%' font-size='#{@size * 0.8}' text-anchor='middle' fill='white' dy='.3em'>#{@initials}</text>
         </svg>
       SVG
-      svg.html_safe # Ensure the output is HTML safe for Rails
+      svg.html_safe
     end
 
-    def self.generate_avatar(name)
-      new(name).generate
+    def self.generate_avatar(name, colors: DEFAULT_COLORS, size: DEFAULT_SIZE)
+      new(name, colors: colors, size: size).generate
     end
 
     private
 
     def extract_initials(name)
-      name.split.map(&:chr).join.upcase # Get initials from the name
+      name.split.map(&:chr).join.upcase
     end
   end
 end
--- lib/calvatar_helper.rb
+++ lib/calvatar_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module CalvatarHelper
-  def calvatar_for(name)
-    Calvatar::Avatar.generate_avatar(name)
+  def calvatar_for(name, colors: Calvatar::Avatar::DEFAULT_COLORS, size: Calvatar::Avatar::DEFAULT_SIZE)
+    Calvatar::Avatar.generate_avatar(name, colors: colors, size: size)
   end
 end
